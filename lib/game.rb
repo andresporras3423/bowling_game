f0d5889f7761e2fcb6a08019c9d2e1267474434f
oscarporras02 <oscarrussi@outlook.com>
2021-06-12 15:08:02 -0500

    add validations for valid values on the score

--- lib/game.rb
+++ lib/game.rb
@@ -4,8 +4,9 @@ require_relative 'player'
 
 class Game
   attr_reader :players
-
   def initialize(relative_path)
+    @valid_options = (0..10).map{|i| [i.to_s,i]}.to_h
+    @valid_options['F']=0
     @players = {}
     absolute_path = File.join(File.dirname(__FILE__), relative_path)
     content_file = File.open(absolute_path)
@@ -13,7 +14,11 @@ class Game
     files_lines.each_line do |line|
       scoreData = line.split(' ')
       player_name = scoreData[0]
-      player_score = scoreData[1]
+      player_score =  @valid_options[scoreData[1]]
+      if player_score.nil?
+        puts "Score should be an integer between 1 and 10"
+        return
+      end
       @players[player_name] = Player.new(player_name) if @players[player_name].nil?
       @players[player_name].scores.push(player_score)
     end
