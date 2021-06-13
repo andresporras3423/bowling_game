065fba6cc9fa683c18fb8b29c3acfbe890fe0ced
oscarporras02 <oscarrussi@outlook.com>
2021-06-13 16:42:25 -0500

    create method for read each line content

--- lib/game.rb
+++ lib/game.rb
@@ -4,35 +4,47 @@ require_relative 'player'
 
 class Game
   attr_reader :players
+
   def initialize(relative_path)
-    @valid_options = (0..10).map{|i| [i.to_s,i]}.to_h
-    @valid_options['F']=0
-    @players = {}
-    absolute_path = File.join(File.dirname(__FILE__), relative_path)
-    content_file = File.open(absolute_path)
-    files_lines = content_file.read
-    files_lines.each_line do |line|
-      scoreData = line.split(' ')
-      player_name = scoreData[0]
-      player_score =  @valid_options[scoreData[1]]
-      if player_score.nil?
-        puts "Score should be an integer between 1 and 10"
-        return
-      end
-      @players[player_name] = Player.new(player_name) if @players[player_name].nil?
-      message=@players[player_name].add_score(player_score)
-      if message!=""
-        puts message
-        return
-      end
+    error_message = read_file_content(relative_path)
+    if error_message != ''
+      puts error_message
+      return
     end
-    content_file.close
-    output=[]
+    output = []
     @players.each_value do |player|
       puts "Player name: #{player.name}"
       player.scores.each do |score|
         puts "frame: #{score.frame}, attempt: #{score.attempt}, point: #{score.points}, total score: #{player.global_scores[score.frame]}"
       end
     end
+  end
+
+  def read_file_content(relative_path)
+    @valid_options = (0..10).map { |i| [i.to_s, i] }.to_h
+    @valid_options['F'] = 0
+    @players = {}
+    absolute_path = File.join(File.dirname(__FILE__), relative_path)
+    content_file = File.open(absolute_path)
+    files_lines = content_file.read
+    files_lines.each_line do |line|
+      error_message = add_line_content(line)
+      return error_message if error_message != ''
+    end
+    content_file.close
+    ''
+  end
+
+  def add_line_content(line)
+    scoreData = line.split(' ')
+    player_name = scoreData[0]
+    player_score = @valid_options[scoreData[1]]
+    return 'Score should be an integer between 1 and 10' if player_score.nil?
+
+    @players[player_name] = Player.new(player_name) if @players[player_name].nil?
+    message = @players[player_name].add_score(player_score)
+    return message if message != ''
+
+    ''
   end
 end
--- lib/player.rb
+++ lib/player.rb
@@ -10,45 +10,48 @@ class Player
     @name = name
     @total_score = 0
     @scores = []
-    @global_scores = (1..10).map{|n| [n, 0]}.to_h
+    @global_scores = (1..10).map { |n| [n, 0] }.to_h
   end
 
   def add_score(points)
-    if @scores.length==0
-        @scores.push(Score.new(points, 1, 1))
-    elsif @scores[-1].frame<=9 || (@scores[-1].frame==9 && (@scores[-1].points==10 || @score[-1].attempt==2)) 
-        if(@scores[-1].attempt==1 && @scores[-1].points<10)
-           return "two attempts in the same frame cannot surpass the 10 points" if @scores[-1].points+points>10
-           @scores.push(Score.new(points, @scores[-1].frame, 2))
-        elsif
-           @scores.push(Score.new(points, @scores[-1].frame+1, 1))
-        end
-    elsif @scores[-1].frame==10 && (@scores[-1].attempt==3 || (@scores[-1].attempt==2 && @scores[-1].points+@scores[-2].points<10))
-        return "current player didn't have more attempts or frames available"
+    if @scores.length.zero?
+      @scores.push(Score.new(points, 1, 1))
+    elsif @scores[-1].frame <= 9 || (@scores[-1].frame == 9 && (@scores[-1].points == 10 || @score[-1].attempt == 2))
+      if @scores[-1].attempt == 1 && @scores[-1].points < 10
+        return 'two attempts in the same frame cannot surpass the 10 points' if @scores[-1].points + points > 10
+
+        @scores.push(Score.new(points, @scores[-1].frame, 2))
+      elsif @scores.push(Score.new(points, @scores[-1].frame + 1, 1))
+      end
+    elsif @scores[-1].frame == 10 && (@scores[-1].attempt == 3 || (@scores[-1].attempt == 2 && @scores[-1].points + @scores[-2].points < 10))
+      return "current player didn't have more attempts or frames available"
     else
-        @scores.push(Score.new(points, 10, @scores[-1].frame==9 ? 1 : @scores[-1].attempt+1))
+      @scores.push(Score.new(points, 10, @scores[-1].frame == 9 ? 1 : @scores[-1].attempt + 1))
     end
-    global_game_score if @scores[-1].frame==10 && (@scores[-1].attempt==3 || @scores[-1].points+@scores[-2].points<10)
-    return ""
+    if @scores[-1].frame == 10 && (@scores[-1].attempt == 3 || @scores[-1].points + @scores[-2].points < 10)
+      global_game_score
+    end
+    ''
   end
 
-  private 
+  private
 
   def global_game_score
     (0...@scores.length).each do |i|
-      current_score= @scores[i].frame==1 ? 0 : @global_scores[@scores[i].frame-1]
-      if(@scores[i].frame<10)
-        if @scores[i].frame<@scores[i+1].frame
-          if @scores[i].attempt==1
-            @global_scores[@scores[i].frame]=@scores[i].points+@scores[i+1].points+@scores[i+2].points+current_score
-          elsif @scores[i].attempt==2 && (@scores[i-1].points+@scores[i].points==10)
-            @global_scores[@scores[i].frame]=@scores[i-1].points+@scores[i].points+@scores[i+1].points+current_score
-          else
-            @global_scores[@scores[i].frame]=@scores[i-1].points+@scores[i].points+current_score
-          end
+      current_score = @scores[i].frame == 1 ? 0 : @global_scores[@scores[i].frame - 1]
+      if @scores[i].frame < 10
+        if @scores[i].frame < @scores[i + 1].frame
+          @global_scores[@scores[i].frame] = if @scores[i].attempt == 1
+                                               @scores[i].points + @scores[i + 1].points + @scores[i + 2].points + current_score
+                                             elsif @scores[i].attempt == 2 && (@scores[i - 1].points + @scores[i].points == 10)
+                                               @scores[i - 1].points + @scores[i].points + @scores[i + 1].points + current_score
+                                             else
+                                               @scores[i - 1].points + @scores[i].points + current_score
+                                             end
         end
-      elsif @scores[i].frame==10 && @scores[i].attempt==1
-        @global_scores[@scores[i].frame]=@scores[i].points+@scores[i+1].points+(@scores[i+2].nil? ? 0 : @scores[i+2].points)+current_score
+      elsif @scores[i].frame == 10 && @scores[i].attempt == 1
+        @global_scores[@scores[i].frame] =
+          @scores[i].points + @scores[i + 1].points + (@scores[i + 2].nil? ? 0 : @scores[i + 2].points) + current_score
       end
     end
   end
