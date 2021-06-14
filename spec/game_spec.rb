368d245f8fc70260de6ee6fde98c382a53c887e5
oscarporras02 <oscarrussi@outlook.com>
2021-06-14 14:41:07 -0500

    add test to game class for valid score

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -5,33 +5,39 @@ RSpec.describe Game do
   describe 'read file content' do
     it 'test invalid content by score which is not a number' do
       (expect do
-        Game.new('./../spec/data_test/invalid_score1.txt', true)
+        Game.new('./../spec/data_test/invalid_score1.txt', false)
       end
       ).to output("Score should be an integer between 1 and 10\n").to_stdout
     end
     it 'test invalid content by score which is a number below 0' do
       (expect do
-        Game.new('./../spec/data_test/invalid_score2.txt', true)
+        Game.new('./../spec/data_test/invalid_score2.txt', false)
       end
       ).to output("Score should be an integer between 1 and 10\n").to_stdout
     end
     it 'test invalid content by score which is a number above 10' do
       (expect do
-        Game.new('./../spec/data_test/invalid_score3.txt', true)
+        Game.new('./../spec/data_test/invalid_score3.txt', false)
       end
       ).to output("Score should be an integer between 1 and 10\n").to_stdout
     end
     it 'test invalid content by a line with only one word' do
       (expect do
-        Game.new('./../spec/data_test/invalid_score4.txt', true)
+        Game.new('./../spec/data_test/invalid_score4.txt', false)
       end
       ).to output("Every line should contain two values, the player's name and the pinfalls\n").to_stdout
     end
     it 'test invalid content by a line with only three words' do
       (expect do
-        Game.new('./../spec/data_test/invalid_score5.txt', true)
+        Game.new('./../spec/data_test/invalid_score5.txt', false)
       end
       ).to output("Every line should contain two values, the player's name and the pinfalls\n").to_stdout
+    end
+    it 'test valid data' do
+      (expect do
+        Game.new('./../spec/data_test/valid_score1.txt', false)
+      end
+      ).to output("").to_stdout
     end
   end
 end
