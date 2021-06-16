87a174ce2a67fcdbe65f9dc06e23500e8ed794f8
oscarporras02 <oscarrussi@outlook.com>
2021-06-16 11:25:02 -0500

    pass a path to the text file as input

--- bin/main.rb
+++ bin/main.rb
@@ -1,4 +1,6 @@
 # frozen_string_literal: true
 
 require_relative '../lib/Game'
-Game.new('../data/scores.txt', true)
+puts 'type the relative path to the file with the game data:'
+relative_path = gets.chomp # use the next valid file:../data/scores.txt
+Game.new(relative_path, true)
